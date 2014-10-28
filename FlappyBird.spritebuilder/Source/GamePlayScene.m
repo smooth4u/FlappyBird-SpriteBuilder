9849067152808b2254343aa4bea009382e32d327
smooth4u <bk@bks-MacBook-Pro.local>
2014-10-27 21:58:52 -0400

    Revert 70f2ca2..052ba64
    This rolls back to commit 70f2ca2f20588388c9585fa9a8a987a2329ce87e.

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -20,7 +20,6 @@
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
     [character flap];
-    [self addObstacle];
 }
 
 
