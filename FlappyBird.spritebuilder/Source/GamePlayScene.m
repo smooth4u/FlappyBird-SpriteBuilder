91725dc3af4fa30b2722735d7a574f0c60236744
smooth4u <bk@bks-MacBook-Pro.local>
2014-10-27 22:03:07 -0400

    Revert 636ec88..021270c
    This rolls back to commit 636ec88754643996bf43aaff7a4b0565fadddb24.

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,17 +9,24 @@
     // your code here
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
+    [self addObstacle];
+    [timeSinceObstacle = 0.0f];
+    [self addObstacle];
+    
 }
 
 -(void)update:(CCTime)delta
 {
-    // put update code here
-}
+    timeSinceObstacle += delta;
+    if (timeSinceObstacle > 2.0f)
+    timeSinceObstacle = 0.0f;
+    }
 
 // put new methods here
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
     [character flap];
+   
 }
 
 
